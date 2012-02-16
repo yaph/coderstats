914bd518aa31676ac40c050549200813f6c481b1
Ramiro Gomez <web@ramiro.org>
2012-02-16 21:30:06 +0100

    database authetication

--- db.rb
+++ db.rb
@@ -6,6 +6,6 @@ dbuser = ENV['OPENSHIFT_DB_USERNAME']
 dbpass = ENV['OPENSHIFT_DB_PASSWORD']
 
 db = Mongo::Connection.new(dbhost, dbport).db('coderstats')
-#auth = db.authenticate(dbuser, dbpass)
+auth = db.authenticate(dbuser, dbpass)
 
 db.collection_names.each { |name| puts name }
