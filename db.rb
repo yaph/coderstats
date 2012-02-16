1904fc1fe55c973865fdd48d49965524a553d92b
Ramiro Gomez <web@ramiro.org>
2012-02-16 23:08:11 +0100

    removed output

--- db.rb
+++ db.rb
@@ -6,8 +6,6 @@ dbport = ENV['OPENSHIFT_NOSQL_DB_PORT']
 dbuser = ENV['OPENSHIFT_NOSQL_DB_USERNAME']
 dbpass = ENV['OPENSHIFT_NOSQL_DB_PASSWORD']
 
-puts dbhost, dbport, dbuser
-
 db = Mongo::Connection.new(dbhost, dbport).db('coderstats')
 auth = db.authenticate(dbuser, dbpass)
 
