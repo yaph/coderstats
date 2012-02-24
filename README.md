2d21cb5298096a643e6caf27b34bc6ebc446a945
Ramiro Gomez <web@ramiro.org>
2012-02-24 16:47:19 +0100

    added logo images, validate user form with html5, dont load app.js, renamed chart to profile

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ http://coderstats.geeksta.net/
 
 * Add icon which uses graphic http://www.iconfinder.com/icondetails/43254/128/chart_graph_monotone_icon
 * Don't display empty tabs
-* Generate 3 different top coder indexes with 100 entries once a day using cron
+* Generate 3 different top coder indexes with 100 entries once a day using cron or once on demand
 * Fix Github oauth login
 * opt-out feature for authenticated users, don't show data if user opted out
+* Aggregations:
+    * overall most used languages of owned repos
+* More details on repos like collaborators http://developer.github.com/v3/repos/collaborators/
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,33 @@
 $(function(){
   $('#ghuser').focus(function(){this.value='';});
 });
+
+//    <div class="row">
+//      <div class="span3">
+//        <header><h2>Public Activity</h2></header>
+//        <div id="ghuser_feed"></div>
+//      </div>
+//    </div>
+var Feed = {
+  load: function() {
+    // users public activity
+    var url = 'https://github.com/' + $('#ghlogin').text() + '.atom';
+    $.getScript('http://www.google.com/jsapi?key=AIzaSyB75IDBFMpF9DsLFbDVwVY0Kzn9CIBBQhY', function(data, textStatus){
+      function initfeed() {
+        var feed = new google.feeds.Feed(url);
+        feed.load(function(result) {
+          if (!result.error) {
+            var container = document.getElementById('ghuser_feed');
+            for (var i = 0; i < result.feed.entries.length; i++) {
+              var entry = result.feed.entries[i];
+              var div = document.createElement('div');
+              div.appendChild(document.createTextNode(entry.title));
+              container.appendChild(div);
+            }
+          }
+        });
+      }
+      google.load('feeds', '1', initfeed);
+    });
+  }
+};
